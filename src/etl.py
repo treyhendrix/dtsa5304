668255057361549bb861269e7e00d337ec27987f
Trey Hendrix <treyhendrix714@gmail.com>
2026-08-23 22:53:03 -0400

    Safer prop calc and TODOs for tomorrow

--- src/etl.py
+++ src/etl.py
@@ -9,6 +9,7 @@ Date Updated: 2026-08-23
 import sys
 from pathlib import Path
 
+import numpy as np
 import scipeds
 from scipeds.constants import COMPLETIONS_TABLE
 from scipeds.data.completions import CompletionsQueryEngine
@@ -40,14 +41,14 @@ if len(duckdb_files) == 0:
     print("No DuckDB files found. Downloading IPEDS data...")
     scipeds.download_db(DATA_DIR, overwrite=True, verbose=False)
     print("IPEDS data sucessfully downloaded as a .duckdb file")
-    duck_db_files = _update_list_of_duckdb_files()
+    duckdb_files = _update_list_of_duckdb_files()
 elif len(duckdb_files) == 1:
     print("Relying on previous IPEDS download.")
 else:
     raise FileExistsError(
         f"Multiple .duckdb files found in data directory: {"; ".join(str(x.name) for x in duckdb_files)}. Please clear the directory and try again."
     )
-DUCK_DB_FILEPATH = duck_db_files[0]
+DUCK_DB_FILEPATH = duckdb_files[0]
 # %% Query the DB for completion data
 engine = CompletionsQueryEngine(DUCK_DB_FILEPATH)
 
@@ -94,29 +95,20 @@ prop_cs_df = cs_df.merge(
 prop_cs_df["cs_bachelors_degrees"] = prop_cs_df["cs_bachelors_degrees"].fillna(0)
 prop_cs_df["bachelors_degrees"] = prop_cs_df["bachelors_degrees"].fillna(0)
 
-# TODO 2026-08-23T20:31:08-0400 Need to make this calculation safe to divide-by-zero errors
 prop_cs_df["prop_cs"] = (
-    prop_cs_df["cs_bachelors_degrees"] / prop_cs_df["bachelors_degrees"]
+    (prop_cs_df["cs_bachelors_degrees"] / prop_cs_df["bachelors_degrees"])
+    .replace(
+        [np.inf, -np.inf], np.nan
+    )  # handle divide-by-zero errors (none are expected)
+    .fillna(0)
 )
 
-
-# Summarize by state
-
-
-# %% Check what CIP codes map to ncses_detailed_field_group
-# TODO 2026-08-23T20:33:57-0400 Working on this
-
-cip_df = engine.get_df_from_query(
-    f"""
-    SELECT DISTINCT 
-        cipcode::TEXT AS cipcode, 
-        ncses_detailed_field_group::TEXT AS ncses_detailed_field_group
-    FROM {COMPLETIONS_TABLE};
-    """
+# %% Summarize by state
+state_all_time_df = (
+    prop_cs_df.groupby("state_abbreviation")
+    .agg(cs_bach=("cs_bachelors_degrees", "sum"), bach=("bachelors_degrees", "sum"))
+    .reset_index()
 )
-cs_cip_df = cip_df.loc[cip_df["ncses_detailed_field_group"] == "Computer Science"]
-# CIP families
-# Mostly 11 = Computer and information Science and Support Services
-# But a few 07 = Does not exist in current taxonomy... weird
-# A few 30 = Multi/Interdisciplinary (e.g., data science)
-# A few 52 = Business (e.g., "Business Systems Networking and Telecommunications")
+state_all_time_df["prop"] = state_all_time_df["cs_bach"] / state_all_time_df["bach"]
+# TODO 2026-08-23T22:45:22-0400 Move this to the qmd?
+# TODO 2026-08-23T22:49:52-0400 Refactor to a main() function that returns prop_cs_df?
